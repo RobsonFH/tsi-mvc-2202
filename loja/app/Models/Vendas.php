e6b54b19a693987ab1f6e68b7e3f3dcde36dfeb0
ROBSON FERREIRA HERCULANO JUNIOR <robson.fhjunior@senacsp.edu.br>
2022-08-23 22:10:30 -0300

    criando models

---
+++ loja/app/Models/Vendas.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vendas extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['id','cleinte_id','vendedor_id'];
+
+    protected $table = 'vendas';
+
+}
