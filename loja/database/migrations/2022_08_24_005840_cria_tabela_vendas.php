7523791124ac313b9c5628e590d232fe082f8c0d
Robson Ferreira <99373533+RobsonFH@users.noreply.github.com>
2022-09-27 16:49:10 -0300

    Update 2022_08_24_005840_cria_tabela_vendas.php

--- loja/database/migrations/2022_08_24_005840_cria_tabela_vendas.php
+++ loja/database/migrations/2022_08_24_005840_cria_tabela_vendas.php
@@ -13,13 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('vendas', function (Blueprint $table) {
+        Schema::create('vendas', function (Blueprint $table) { //criando um Schema onde mostra todos os campos que contem na tabela personagens.
             $table->id();
             $table->timestamps();
-            $table->bigInteger('cliente_id')->unsigned();
-            $table->foreign('cliente_id')->references('id')->on('clientes')->onDelete('cascade');
-            $table->bigInteger('vendedor_id')->unsigned();
-            $table->foreign('vendedor_id')->references('id')->on('vendedores')->onDelete('cascade');
+            $table->bigInteger('cliente_id')->unsigned(); //Deixa só positivos, e não deixa criar numeros negativos.
+            $table->foreign('cliente_id')->references('id')->on('clientes')->onDelete('cascade');//Criando uma chave estrangeira da coluna id, da tabela clientes, deletando em cascata (ex: deletando o usuario tudo dele sera deletado)
+            $table->bigInteger('vendedor_id')->unsigned(); //Deixa só positivos, e não deixa criar numeros negativos.
+            $table->foreign('vendedor_id')->references('id')->on('vendedores')->onDelete('cascade');//Criando uma chave estrangeira da coluna id, da tabela vendedor, deletando em cascata (ex: deletando o usuario tudo dele sera deletado)
 
 
         });
@@ -30,7 +30,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down() //se rodar esse comando ele deleta a tabela.
     {
         Schema::dropIfExists('vendas');
     }
