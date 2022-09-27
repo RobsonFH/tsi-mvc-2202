9a6e8bbd7bc1f5a4474be06a423b21f5299586ea
Robson Ferreira <99373533+RobsonFH@users.noreply.github.com>
2022-09-27 16:58:30 -0300

    Update Clientes.php

--- loja/app/Models/Clientes.php
+++ loja/app/Models/Clientes.php
@@ -14,7 +14,7 @@ class Clientes extends Model
 
     protected $table = 'clientes';
 
-
+//relacionamento entre tabelas
     public function compras(){
 
         return $this->hasMany( Vendas::class, 'cliente_id');
