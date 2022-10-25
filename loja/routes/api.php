305b4dab613005bcc5a66d7a33bef269c50da411
ROBSON FERREIRA HERCULANO JUNIOR <robson.fhjunior@senacsp.edu.br>
2022-10-25 20:50:58 -0300

    API

--- loja/routes/api.php
+++ loja/routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(static function(){ //prefixo para todos os endpoints
+
+    Route::get('/vendedores', [App\Http\Controllers\vendedoresApiController::class, 'index']); //get para obter informações, chama o método index da api controller.
+    Route::post('/vendedores', [App\Http\Controllers\vendedoresApiController::class, 'store']);
+    Route::delete('/vendedores/{id}', [App\Http\Controllers\vendedoresApiController::class, 'destroy']);
+    Route::put('/vendedores/{id}', [App\Http\Controllers\vendedoresApiController::class, 'update']);
+
+
+});
